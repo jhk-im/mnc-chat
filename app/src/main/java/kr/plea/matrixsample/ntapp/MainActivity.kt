7e536add65353807ffcf125a969e010e522f8daa
coderjhk <wanted0301@gmail.com>
2021-10-25 18:35:15 +0900

    first commit

---
+++ app/src/main/java/kr/plea/matrixsample/ntapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package kr.plea.matrixsample.ntapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
